a6fcf2bcc24a6dc83e091262e6c7b6ca0d82120b
nikolaiser <mail@nikolaiser.com>
2024-12-07 23:39:00 +0100

    Add prusa slicer config

--- nixos/modules/cad/home/default.nix
+++ nixos/modules/cad/home/default.nix
@@ -15,10 +15,17 @@
   ];
 
   # https://github.com/danth/stylix/pull/264
-  xdg.dataFile = {
-    "FreeCAD/Mod/Stylix/Stylix/Stylix.cfg".text = import ./freecad/cfg.nix config lib;
-    "FreeCAD/Mod/Stylix/Stylix/Stylix.qss".text = import ./freecad/stylesheet.nix config;
-    "FreeCAD/Mod/Stylix/package.xml".text = import ./freecad/package.nix config;
+  xdg = {
+    dataFile = {
+      "FreeCAD/Mod/Stylix/Stylix/Stylix.cfg".text = import ./freecad/cfg.nix config lib;
+      "FreeCAD/Mod/Stylix/Stylix/Stylix.qss".text = import ./freecad/stylesheet.nix config;
+      "FreeCAD/Mod/Stylix/package.xml".text = import ./freecad/package.nix config;
+    };
+    configFile.PrusaSlicer = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/dungeon/nixos/modules/cad/home/prusa-slicer";
+      recursive = true;
+    };
+
   };
 
 }
