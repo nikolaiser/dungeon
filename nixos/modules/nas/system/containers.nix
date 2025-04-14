76530e755bd8201f49d88063fe2490960fd59915
nikolaiser <mail@nikolaiser.com>
2025-04-14 18:07:49 +0200

    Add container auto upgrade

---
+++ nixos/modules/nas/system/containers.nix
@@ -0,0 +1,56 @@
+{ pkgs, ... }:
+
+let
+  update-containers = pkgs.writeShellScriptBin "update-containers" ''
+    	SUDO=""
+    	if [[ $(id -u) -ne 0 ]]; then
+    		SUDO="sudo"
+    	fi
+
+        images=$($SUDO ${pkgs.podman}/bin/podman ps -a --format="{{.Image}}" | sort -u)
+
+        for image in $images
+        do
+          $SUDO ${pkgs.podman}/bin/podman pull $image
+        done
+  '';
+in
+
+{
+  environment.systemPackages = [ update-containers ];
+
+  systemd = {
+    timers = {
+      updatecontainers = {
+        timerConfig = {
+          Unit = "updatecontainers.service";
+          OnCalendar = "Mon 02:00";
+        };
+        wantedBy = [ "timers.target" ];
+      };
+      restartcontainers = {
+        timerConfig = {
+          Unit = "restartcontainers.service";
+          OnCalendar = "Mon 03:00";
+        };
+        wantedBy = [ "timers.target" ];
+      };
+    };
+
+    services = {
+      updatecontainers = {
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${update-containers}/bin/update-containers";
+        };
+      };
+      restartcontainers = {
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "sudo ${pkgs.podman}/bin/podman restrt --all";
+        };
+      };
+    };
+  };
+
+}
