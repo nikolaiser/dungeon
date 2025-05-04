e3fe523ac6648254ba40b9a4c39b9fc6ce69122f
nikolaiser <mail@nikolaiser.com>
2025-05-04 19:01:12 +0200

    Add swagger editor

---
+++ nixos/modules/nas/system/tools.nix
@@ -0,0 +1,40 @@
+{ config, ... }:
+
+let
+  itToolsUrl = "it-tools.${config.nas.baseDomain.private}";
+  swaggerEditorUrl = "swagger-editor.${config.nas.baseDomain.private}";
+in
+{
+  virtualisation.oci-containers.containers = {
+    it-tools = {
+      image = "corentinth/it-tools:latest";
+      autoStart = true;
+      ports = [ "9191:80" ];
+    };
+    swagger-editor = {
+      image = "swaggerapi/swagger-editor:latest";
+      autoStart = true;
+      ports = [ "9192:8080" ];
+    };
+  };
+
+  services.nginx.virtualHosts = {
+    ${itToolsUrl} = {
+      forceSSL = true;
+      useACMEHost = "${config.nas.baseDomain.private}";
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:9191";
+        proxyWebsockets = true;
+      };
+    };
+    ${swaggerEditorUrl} = {
+      forceSSL = true;
+      useACMEHost = "${config.nas.baseDomain.private}";
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:9192";
+        proxyWebsockets = true;
+      };
+    };
+  };
+
+}
